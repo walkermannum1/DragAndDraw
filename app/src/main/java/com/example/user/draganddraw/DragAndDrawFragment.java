aad99679304d906fdce84a26658382e374f34083
walkermannum1 <2469377884@qq.com>
2016-09-12 20:28:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,12 @@
+package com.example.user.draganddraw;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by user on 2016/9/12.
+ */
+public class DragAndDrawFragment extends Fragment {
+    public static DragAndDrawFragment newInstance() {
+        return new DragAndDrawFragment();
+    }
+}
