076d2231b4c91c0b0e4fa93708007260fa676c16
walkermannu1 <2469377884@qq.com>
2016-09-16 18:00:48 +0800

    Add some code.

---
+++ app/src/main/java/com/example/user/draganddraw/BoxDrawingView.java
@@ -0,0 +1,22 @@
+package com.example.user.draganddraw;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 24693 on 2016/9/16.
+ */
+public class BoxDrawingView extends View {
+
+    //Used when creating the view in code
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    //
+    public BoxDrawingView(Context context, AttributeSet attr) {
+        super(context,attr);
+    }
+}
--- app/src/main/java/com/example/user/draganddraw/DragAndDrawFragment.java
+++ app/src/main/java/com/example/user/draganddraw/DragAndDrawFragment.java
@@ -1,6 +1,10 @@
 package com.example.user.draganddraw;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by user on 2016/9/12.
@@ -8,5 +12,11 @@ import android.support.v4.app.Fragment;
 public class DragAndDrawFragment extends Fragment {
     public static DragAndDrawFragment newInstance() {
         return new DragAndDrawFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
+        return v;
     }
 }
