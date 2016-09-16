5813270ac28e2f8fbc074f261a526179c026bafa
walkermannu1 <2469377884@qq.com>
2016-09-16 20:45:00 +0800

    Get point and motion.

--- app/src/main/java/com/example/user/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/example/user/draganddraw/BoxDrawingView.java
@@ -2,13 +2,17 @@ package com.example.user.draganddraw;
 
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
- * Created by 24693 on 2016/9/16.
+ * Created by guang on 2016/9/16.
  */
 public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
 
     //Used when creating the view in code
     public BoxDrawingView(Context context) {
@@ -18,5 +22,30 @@ public class BoxDrawingView extends View {
     //
     public BoxDrawingView(Context context, AttributeSet attr) {
         super(context,attr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(),event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "CANCEL";
+                break;
+        }
+
+        Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+
+        return true;
     }
 }
